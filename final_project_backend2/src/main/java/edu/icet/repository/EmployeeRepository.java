e7c9ee116503f14d603ddea02b1e263fac7f94ae
amashikaushala <amashikaushala2003@gmail.com>
2025-04-29 13:39:22 +0530

    first commit

---
+++ final_project_backend2/src/main/java/edu/icet/repository/EmployeeRepository.java
@@ -0,0 +1,10 @@
+package edu.icet.repository;
+
+import edu.icet.entity.Employee;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.Optional;
+
+public interface EmployeeRepository extends JpaRepository<Employee, Long> {
+    Optional<Employee> findByEmail(String email);
+}
